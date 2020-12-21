a2e3776894673341bb5e541284029a188bd8c8a7
rsantalla <69609111+rsantalla@users.noreply.github.com>
2020-12-21 13:23:38 -0300

    Add files via upload

--- src/main/java/ar/edu/davinci/dvds20202cg5/repository/ItemRepository.java
+++ src/main/java/ar/edu/davinci/dvds20202cg5/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package ar.edu.davinci.dvds20202cg5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.davinci.dvds20202cg5.model.Item;
 
